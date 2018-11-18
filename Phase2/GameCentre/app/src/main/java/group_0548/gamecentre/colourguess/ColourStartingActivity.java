8457cd08af2ac0fa9cd75e8821484247623da42b
Yiqing Jin <bruce.jin@mail.utoronto.ca>
2018-11-17 19:47:41 -0500

    Added new activity.

---
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/colourguess/ColourStartingActivity.java
@@ -0,0 +1,15 @@
+package group_0548.gamecentre.colourguess;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import group_0548.gamecentre.R;
+
+public class ColourStartingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colour_starting);
+    }
+}
