2d1c1aec56d9b2c36e137f3f93f3e6f1b8c22d9d
Yiqing Jin <bruce.jin@mail.utoronto.ca>
2018-11-20 17:06:55 -0500

    Added new activity class to make the scoreboard activity more general.

---
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/ChooseScoreBoardActivity.java
@@ -0,0 +1,109 @@
+package group_0548.gamecentre;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+
+import group_0548.gamecentre.colourguess.ColourGuessStartingActivity;
+import group_0548.gamecentre.slidingtiles.StartingActivity;
+
+/**
+ * The scoreboard activity.
+ */
+public class ChooseScoreBoardActivity extends AppCompatActivity {
+
+    /**
+     * List of user names.
+     */
+    public static String userNames;
+
+    /**
+     * List of user scores.
+     */
+    public static String userScores;
+
+    /**
+     * The game type of the current game.
+     */
+    public static String gameType = null;
+
+
+    /**
+     * Initialize the activity layout that directs the user to different
+     * scoreboards.
+     *
+     * @param savedInstanceState the instance saved
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_score_board);
+        addEasyButtonListener();
+        addMediumButtonListener();
+        addHardButtonListener();
+    }
+
+    /**
+     * Activate the easy button.
+     */
+    private void addEasyButtonListener() {
+        Button returnButton = findViewById(R.id.ScoreBoardEasyButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToScore("Easy");
+            }
+        });
+    }
+
+    /**
+     * Activate the medium button.
+     */
+    private void addMediumButtonListener() {
+        Button returnButton = findViewById(R.id.ScoreBoardMediumButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToScore("Medium");
+            }
+        });
+    }
+
+    /**
+     * Activate the easy button.
+     */
+    private void addHardButtonListener() {
+        Button returnButton = findViewById(R.id.ScoreBoardHardButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToScore("Hard");
+            }
+        });
+    }
+
+    /**
+     * Switch to the scoreboard for easy difficult.
+     */
+    private void switchToScore(String complexity) {
+        ArrayList<String> list = new ArrayList<>();
+        if (gameType.equals("Sliding Tiles")) {
+            list = StartingActivity.scoreBoardManager.getScoreBoard(complexity)
+                    .getScoreContent();
+        } else if (gameType.equals("Colour Guess")) {
+            list = ColourGuessStartingActivity.scoreBoardManager.getScoreBoard(complexity)
+                    .getScoreContentReverse();
+        }
+        if (list.size() != 0) {
+            userNames = list.get(0);
+            userScores = list.get(1);
+        }
+        Intent tep = new Intent(this, ScoreBoardActivity.class);
+        startActivity(tep);
+    }
+}
