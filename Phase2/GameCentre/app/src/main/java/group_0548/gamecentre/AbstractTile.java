1650b17fbf3fd7ac2e00977e5b5c6732a2ea218a
Alexander Shum <alexander.shum@mail.utoronto.ca>
2018-11-20 16:38:15 -0500

    Commited the abstract tile class for other similar tile objects that will be implmented

---
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/AbstractTile.java
@@ -0,0 +1,28 @@
+package group_0548.gamecentre;
+
+/**
+ * Abstract Tile Object for the individual tile object in the game.
+ */
+public class AbstractTile {
+
+    /**
+     * An unique ID for the this abstract tile
+     */
+    private int id;
+
+    /**
+     * Return the ID of this abstract tile
+     * @return the ID of this tile
+     */
+    public int getId() {
+        return id;
+    }
+
+
+
+
+
+
+
+
+}
