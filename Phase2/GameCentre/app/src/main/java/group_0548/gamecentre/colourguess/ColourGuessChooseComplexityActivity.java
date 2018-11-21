ff14c9a821060e42ec78a4dd81843f1183df9f4d
Yiqing Jin <bruce.jin@mail.utoronto.ca>
2018-11-21 11:45:00 -0500

    Making the back button go back to ColourGuessStartingActivity to avoid unwanted behaviour.

--- Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/colourguess/ColourGuessChooseComplexityActivity.java
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/colourguess/ColourGuessChooseComplexityActivity.java
@@ -1,17 +1,14 @@
 package group_0548.gamecentre.colourguess;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.util.Observable;
 
 import group_0548.gamecentre.R;
 
@@ -101,6 +98,13 @@ public class ColourGuessChooseComplexityActivity extends AppCompatActivity {
         } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, ColourGuessStartingActivity.class));
+        finish();
     }
 }
 
