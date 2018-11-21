0f9bdc2862f37efb4c34df23480d33bb705a8414
Yiqing Jin <bruce.jin@mail.utoronto.ca>
2018-11-20 23:47:26 -0500

    Making AbstractTile implement observable.

--- Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/AbstractManager.java
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/AbstractManager.java
@@ -1,8 +1,9 @@
 package group_0548.gamecentre;
 
 import java.io.Serializable;
+import java.util.Observable;
 
-public abstract class AbstractManager implements Serializable {
+public abstract class AbstractManager extends Observable implements Serializable {
 
     /**
      * The board being managed.
@@ -21,6 +22,11 @@ public abstract class AbstractManager implements Serializable {
     public abstract String getComplexity();
 
     public abstract String getGameType();
+
+    public void changeAndNotify(){
+        setChanged();
+        notifyObservers();
+    }
 
 
 }
