5fc449cf14f783da2bcbd1460905b991b319b157
Yiqing Jin <bruce.jin@mail.utoronto.ca>
2018-11-20 20:02:01 -0500

    Adding a button for the new game.

--- Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/GameCentreActivity.java
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/GameCentreActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import group_0548.gamecentre.slidingtiles.StartingActivity;
+import group_0548.gamecentre.colourguess.ColourGuessStartingActivity;
 
 /**
  * The GameCentreActivity class where all the available games can be viewed and selected
@@ -22,6 +23,7 @@ public class GameCentreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_centre);
         addStartSlidingTilesListener();
+        addStartColourGuessListener();
     }
 
     /**
@@ -38,10 +40,31 @@ public class GameCentreActivity extends AppCompatActivity {
     }
 
     /**
+     * Initializing the Colour Guess button to go to the sliding tile game
+     */
+    private void addStartColourGuessListener() {
+        Button button = findViewById(R.id.ColourGuessGame);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToColourGuess();
+            }
+        });
+    }
+
+    /**
      * Method to go the sliding tile game activity page
      */
     private void switchToSlidingTiles() {
         Intent tmp = new Intent(this, StartingActivity.class);
+        startActivity(tmp);
+    }
+
+    /**
+     * Method to go the colour guess game activity page
+     */
+    private void switchToColourGuess() {
+        Intent tmp = new Intent(this, ColourGuessStartingActivity.class);
         startActivity(tmp);
     }
 }
