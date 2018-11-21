bd78c4587719e73765a057d6720f240d6cd59958
Yiqing Jin <bruce.jin@mail.utoronto.ca>
2018-11-20 23:47:06 -0500

    Making AbstractTile implement serializable.

--- Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/AbstractTile.java
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/AbstractTile.java
@@ -1,9 +1,11 @@
 package group_0548.gamecentre;
 
+import java.io.Serializable;
+
 /**
  * Abstract Tile Object for the individual tile object in the game.
  */
-public abstract class AbstractTile {
+public abstract class AbstractTile implements Serializable {
 
     /**
      * An unique ID for the this abstract tile
