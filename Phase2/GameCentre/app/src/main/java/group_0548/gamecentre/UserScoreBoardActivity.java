3beb95b3e986d89785a122b68ad3b4a6e3e80653
Yiqing Jin <bruce.jin@mail.utoronto.ca>
2018-11-20 19:34:40 -0500

    Adding new activity for user scoreboard.

---
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/UserScoreBoardActivity.java
@@ -0,0 +1,13 @@
+package group_0548.gamecentre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserScoreBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_score_board);
+    }
+}
