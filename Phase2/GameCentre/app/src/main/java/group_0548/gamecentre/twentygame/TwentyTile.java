b6f7f5285361f59136971ad8995a62fa3a97cfa9
Waasay Shehzad <w.shehzad@mail.utoronto.ca>
2018-11-24 22:14:05 -0500

    Implemented a tile for 2048 game.

---
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/twentygame/TwentyTile.java
@@ -0,0 +1,33 @@
+package group_0548.gamecentre.twentygame;
+
+import group_0548.gamecentre.AbstractTile;
+import group_0548.gamecentre.R;
+
+public class TwentyTile extends AbstractTile {
+
+    private int id;
+
+    private int background;
+
+    TwentyTile(int id) {
+
+        this.id = id;
+
+        int[] images = {R.drawable.t2, R.drawable.t4, R.drawable.t8, R.drawable.t16,
+                R.drawable.t32, R.drawable.t64, R.drawable.t128, R.drawable.t256, R.drawable.t512,
+                R.drawable.t1024, R.drawable.t2048, R.drawable.background};
+
+        this.background = images[id];
+
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public int getBackground() {
+        return background;
+    }
+
+}
+
