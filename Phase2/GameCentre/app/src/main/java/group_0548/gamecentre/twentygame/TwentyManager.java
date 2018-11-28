b986a6263d790a0f0086c045b5989aed9f4096be
Waasay Shehzad <w.shehzad@mail.utoronto.ca>
2018-11-28 18:21:41 -0500

    Fixed the copying of tiles in swipeDown(). Was originally copying [col][row] to [row][col].

--- Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/twentygame/TwentyManager.java
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/twentygame/TwentyManager.java
@@ -352,8 +352,8 @@ public class TwentyManager extends AbstractManager<TwentyBoard> implements Undoa
         }*/
         for (int col = 0; col != this.getBoard().getNumCol(); col++) {
             for (int row = 0; row < this.getBoard().getNumRow(); row++) {
-                //newTiles[col][row] = newTilesCopy[row][this.getBoard().getNumCol() - 1 - col];
-                newTiles[row][col] = newTilesCopy[col][this.getBoard().getNumRow() - 1 - row];
+                newTiles[col][row] = newTilesCopy[row][this.getBoard().getNumCol() - 1 - col];
+
             }
         }
 
@@ -384,7 +384,7 @@ public class TwentyManager extends AbstractManager<TwentyBoard> implements Undoa
 
         for (int row = 0; row != this.getBoard().getNumRow(); row++) {
             for (int col = 0; col != this.getBoard().getNumRow(); col++) {
-                newTilesCopy1[row][col] = newTiles[col][row];
+                newTilesCopy1[row][col] = newTiles[row][col];
             }
         }
 
@@ -419,7 +419,7 @@ public class TwentyManager extends AbstractManager<TwentyBoard> implements Undoa
 
         for (int row = 0; row != this.getBoard().getNumRow(); row++) {
             for (int col = 0; col != this.getBoard().getNumCol(); col++) {
-                newTilesCopy3[row][col] = newTiles[col][row];
+                newTilesCopy3[row][col] = newTiles[row][col];
             }
         }
 
@@ -433,8 +433,8 @@ public class TwentyManager extends AbstractManager<TwentyBoard> implements Undoa
 
         for (int col = 0; col != this.getBoard().getNumCol(); col++) {
             for (int row = 0; row < this.getBoard().getNumRow(); row++) {
-                //newTiles[col][row] = newTilesCopy3[row][this.getBoard().getNumCol() - 1 - col];
-                newTiles[row][col] = newTilesCopy3[col][this.getBoard().getNumRow() - 1 - row];
+                newTiles[col][row] = newTilesCopy3[row][this.getBoard().getNumCol() - 1 - col];
+
             }
         }
 
