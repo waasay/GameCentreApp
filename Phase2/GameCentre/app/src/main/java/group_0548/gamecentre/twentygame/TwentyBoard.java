b16cc68fa7e0301265f15c0167804cda0c3f7f8a
Waasay Shehzad <w.shehzad@mail.utoronto.ca>
2018-11-30 00:57:19 -0500

    Added JavaDocs

--- Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/twentygame/TwentyBoard.java
+++ Phase2/GameCentre/app/src/main/java/group_0548/gamecentre/twentygame/TwentyBoard.java
@@ -9,13 +9,22 @@ import java.util.Random;
 
 import group_0548.gamecentre.AbstractBoard;
 
-
+/**
+ * A board representing a 2048 board.
+ */
 public class TwentyBoard extends AbstractBoard<TwentyTile> implements Iterable<TwentyTile> {
 
-
+    /**
+     * The tiles representing the board.
+     */
     private TwentyTile[][] tiles;
 
-
+    /**
+     * Create a TwentyBoard representing the board for a 2048 game.
+     *
+     * @param rowNum the number of rows for the board to have
+     * @param colNum the number of columns for the board to have
+     */
     TwentyBoard(int rowNum, int colNum) {
 
         Random randomGen = new Random();
@@ -54,6 +63,13 @@ public class TwentyBoard extends AbstractBoard<TwentyTile> implements Iterable<T
 
     }
 
+    /**
+     * Create a TwentyBoard representing the board for a 2048 game.
+     *
+     * @param tileSet the tiles to fill this board with
+     * @param numRow the number of rows for the board to have
+     * @param numCol the number of columns for the board to have
+     */
     public TwentyBoard(List<TwentyTile> tileSet, int numRow, int numCol) {
         tiles = new TwentyTile[numRow][numCol];
         Iterator<TwentyTile> iterator = tileSet.iterator();
@@ -65,17 +81,33 @@ public class TwentyBoard extends AbstractBoard<TwentyTile> implements Iterable<T
             }
         }
 
-
     }
 
+    /**
+     * Get the tile in a row and column in the board.
+     *
+     * @param row a row of the board
+     * @param col a column of the board
+     * @return the tile in that row and column in the board.
+     */
     public TwentyTile getTile(int row, int col) {
         return this.tiles[row][col];
     }
 
+    /**
+     * Get the tiles representing this board.
+     *
+     * @return the tiles representing the board
+     */
     public TwentyTile[][] getTiles() {
         return this.tiles;
     }
 
+    /**
+     * Set this board to represent newTiles.
+     *
+     * @param newTiles the tiles to set this board to
+     */
     public void setTiles(TwentyTile[][] newTiles) {
         this.tiles = newTiles;
     }
@@ -92,7 +124,11 @@ public class TwentyBoard extends AbstractBoard<TwentyTile> implements Iterable<T
         return new TwentyBoard.BoardIterator();
     }
 
-
+    /**
+     * Return a new TwentyBoard representing the same board as this board.
+     *
+     * @return a new TwentyBoard representing the same board as this board.
+     */
     TwentyBoard copy() {
         List<TwentyTile> newTiles = new ArrayList<>();
         for (TwentyTile t : this) {
@@ -101,7 +137,11 @@ public class TwentyBoard extends AbstractBoard<TwentyTile> implements Iterable<T
         return new TwentyBoard(newTiles, numRow, numCol);
     }
 
-
+    /**
+     * Change this board's tiles to the tiles representing newBoard.
+     *
+     * @param newBoard the board from which to get change the tiles to
+     */
     void replaceBoard(TwentyBoard newBoard) {
         TwentyTile[][] newTiles = newBoard.getTiles();
         this.setTiles(newTiles);
